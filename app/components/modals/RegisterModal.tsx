709d95c9f978d237076f424a82aa4c92bc21589c
Harsh Vardhan Soni <sonihv01@gmail.com>
2025-11-30 14:32:18 +0530

    tenth commit

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,5 +1,16 @@
+"use client";
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+//
+import useRegisterModal from "../../hooks/useRegisterModal";
+
 const RegisterModal = () => {
   return <div></div>;
 };
 
+//1:06:42
 export default RegisterModal;

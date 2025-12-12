1778145dbaac797faf8c5b789f2f4b61dfcc6e32
Harsh Vardhan Soni <sonihv01@gmail.com>
2025-12-12 21:38:32 +0530

    added interface in category

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { IconType } from "react-icons";
+
 interface CategoryBoxProps {
   //
+  icon: IconType;
+  label: string;
+  description?: string;
 }
 
-const CategoryBox = () => {};
+const CategoryBox: React.FC<CategoryBoxProps> = ({ icon, label }) => {
+  return <div></div>;
+};
 
 export default CategoryBox;
